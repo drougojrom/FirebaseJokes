38b9d9e792552f2c6b3ca2a121f6d6273e1a959f
Roman Ustuantsev <liberrom.hse@gmail.com>
2016-04-17 20:38:05 +0300

    updated addJokeViewController

--- FirebaseJokes/AddJokeViewController.swift
+++ FirebaseJokes/AddJokeViewController.swift
@@ -10,9 +10,22 @@ import UIKit
 
 class AddJokeViewController: UIViewController {
     
+    var currentUsername = ""
+    
     @IBOutlet weak var jokeField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // get username of the current user, and set it to currentUsername, so we can add it to the Joke
+        
+        DataService.dataService.CURRENT_USER_REF.observeEventType(FEventType.Value, withBlock: { snapshot in
+            
+            let currentUser = snapshot.value.objectForKey("username") as! String
+            
+            print("Username: \(currentUser)")
+            self.currentUsername = currentUser}, withCancelBlock: {error in
+                print(error.description)
+        })
         
     }
     
@@ -22,6 +35,9 @@ class AddJokeViewController: UIViewController {
     }
     
     @IBAction func saveJoke(sender: AnyObject) {
+        
+        let jokeText = jokeField.text
+        
         
     }
     
